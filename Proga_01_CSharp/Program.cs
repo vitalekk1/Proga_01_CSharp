70b09db2473573bd0b7f72ff2dc02fd8f1bf6668
vitalekk1 <vitalikkakanov55@gmail.com>
2020-04-19 22:00:33 +0500

    Добавьте файлы проекта.

---
+++ Proga_01_CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proga_01_CSharp
+{
+    class Program
+    {
+        private static double x;
+        static void Main(string[] args)
+        {
+            x = Convert.ToDouble(Console.ReadLine());
+            int summ = ((int)(x * 10) % 10) * ((int)(x * 100) % 10);
+            Console.WriteLine(summ);
+            Console.ReadKey();
+        }
+    }
+}
